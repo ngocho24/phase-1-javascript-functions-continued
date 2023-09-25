2eedad93599f46c98aab1375451c3d8ae8469bde
Elijah Ngocho <elijahngocho24@gmail.com>
2023-09-25 18:14:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code your solution here
+function saturdayFun (fun = "roller-skate"){
+    return `This Saturday, I want to ${fun}!`;
+}
+// saturdayFun ();
+
+
+function mondayWork (todo = "go to the office"){
+    return`This Monday, I will ${todo}.`;
+}
+mondayWork ();
+
+
+const adjectiveDeclared = function (style = "*",adj ="special"){
+
+    console.log(`You are ${style}${adj}${style}!`);
+    
+     }
+     adjectiveDeclared ();
+
+
+// function wrapAdjective(word ="||"){
+//     return function(adjective='a dedicated programmer'){
+//         return `You are ${word}${adjective}${word}!`;}}
+
+function wrapAdjective(visualFlair = "*",) {
+    return `You are ${visualFlair} ${adj} ${visualFlair}`
+}
+const encouragingPromptFunction = wrapAdjective("!!!")
+const wrappedMessage = encouragingPromptFunction ( "a dedicated programmer")
+
+console.log (wrappedMessage);
